eea970b6ab3b00fcc0b0326910c249864c0ce0c4
unknown <sintexte@gmail.com>
2022-07-26 04:21:45 +0200

    UPDATE {modify proxy for azure nodejs server} {minor style modification}

--- front-end/src/component/Login.js
+++ front-end/src/component/Login.js
@@ -13,15 +13,20 @@ function Login(){
 
      const  handlesubmit = async (event) =>{
         const form =  event.currentTarget
+        
         if(form.checkValidity() === false){
             event.preventDefault(); 
             event.stopPropagation();
+            
+            setValidated(true)
         }else{
+            
+            setValidated(false)
             event.preventDefault(); 
             event.stopPropagation();
             const username = form.formUsername.value
             const password = form.formPassword.value
-            let good = false
+            setMessage("Vérification ...")
             const response = await fetch('login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
@@ -30,33 +35,31 @@ function Login(){
                     'password': password
                 })
             })
-            if(response.status == 403){
+            if(response.status === 403){
                 setMessage("Mauvais Nom d'utulisateur ou Mot de Passe")
-                setValidated(false)
-            }else if(response.status == 200){
+            }else if(response.status === 200){
                 let parsedres = await response.json()
                 let local_token = parsedres.data.token
                 let id_role = parsedres.data.id_role;
-                if(id_role==0){
+                if(id_role===0){
                     dispatch(setToken(local_token))
                     dispatch(Connected())
                     setMessage("Bienvenue")
                     navigate('/')
                 }else{
-                    setMessage("Mauvais Nom d'utulisateur ou Mot de Passe [FORBIDDEN]")
+                    setMessage("[FORBIDDEN]")
                     setValidated(false)
                 }
             }
         }
-        setValidated(true)
     }
     return(
         <>
             <Container>
                 <Row className="vh-100 d-flex justify-content-center align-items-center">
                 <Col md={8} lg={6} xs={12}>
-                    <div className="border border-3 border-primary"></div>
-                    <Card className="shadow">
+                    <div className="border border-3 border-primary" style={{borderRadius:"7px 7px 0px 0px"}}></div>
+                    <Card className="shadow" style={{borderRadius:"0px 0px 7px 7px"}}>
                     <Card.Body id="login">
                         <div className="mb-3 mt-md-4">
                         <h2 className="fw-bold mb-2 text-uppercase txt-center">Agent Admin</h2>
@@ -73,18 +76,6 @@ function Login(){
                                 >
                                     <Form.Control required  type="password" placeholder="Password" />
                                 </Form.Group>
-                            {/*
-                            <Form.Group
-                                className="mb-3"
-                                controlId="formBasicCheckbox"
-                            >
-                                <p className="small">
-                                <a className="text-primary" href="#!">
-                                    Forgot password?
-                                </a>
-                                </p>
-                            </Form.Group>
-                            */}
                                 <div className="d-grid">
                                     <Button variant="primary" type="submit">
                                         Se Connecter
